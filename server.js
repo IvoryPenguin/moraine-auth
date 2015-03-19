9b26a41a0989a7398ff817a178c1a4fd5b619573
Aaron Maturen <aaron@ivorypenguin.com>
2015-03-18 21:36:21 -0400

    initial commit with only inmemory auth

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    oauthserver = require('oauth2-server'),
+    memorystore = require('./model.js');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+
+app.oauth = oauthserver({
+  model: memorystore,
+  grants: ['password','refresh_token'],
+  debug: true
+});
+
+app.all('/oauth/token', app.oauth.grant());
+
+app.get('/', app.oauth.authorise(), function (req, res) {
+  res.send('Secret area');
+});
+
+app.use(app.oauth.errorHandler());
+
+app.listen(3000);
